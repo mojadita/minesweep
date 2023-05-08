772f13aa2fe0b020eef045c7c5cdd53f77526005
Luis Colorado <luiscoloradourcola@gmail.com>
2023-05-08 22:05:58 +0300

    Add support to open multiple cells
    Signed-off-by: Luis Colorado <luiscoloradourcola@gmail.com>

--- ms/src/main/java/es/lcssl/games/ms/Ms.java
+++ ms/src/main/java/es/lcssl/games/ms/Ms.java
@@ -1,6 +1,6 @@
 /*
  * License file not set.
- * 
+ *
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
@@ -12,11 +12,12 @@ import java.awt.EventQueue;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.util.Random;
-import javax.swing.JPanel;
-import static java.lang.String.format;
 import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import static java.lang.String.format;
 
 /**
  *
@@ -25,6 +26,7 @@ import javax.swing.JFrame;
 public class Ms extends JPanel {
 
     public static final byte MINE = (byte) 0x80;
+    public static final byte ALREADY_OPENED = 0x40;
     public static final byte MINES_MASK = 0x0f;
     public static final double DEFAULT_PROB = 0.12;
     public static final int DEFAULT_ROWS = 10;
@@ -32,42 +34,61 @@ public class Ms extends JPanel {
 
     public static final Color bg[] = {
         Color.WHITE, Color.GREEN, Color.CYAN, Color.YELLOW, Color.ORANGE,
-        Color.PINK, Color.MAGENTA, Color.RED, Color.DARK_GRAY};
+        Color.PINK, Color.MAGENTA, Color.RED, Color.DARK_GRAY };
 
     private final int rows, cols;
 
     private final byte[][] cells;
 
-    public Ms(int rows, int cols, double prob) {
-        super(new GridLayout(rows, cols));
+    private final JButton[][] pbs;
+
+    public Ms( int rows, int cols, double prob ) {
+        super( new GridLayout( rows, cols ) );
 
         this.rows = rows;
         this.cols = cols;
         cells = new byte[rows][cols];
+        pbs = new JButton[rows][cols];
+
         int N = rows * cols, n = (int) (N * prob + 0.5);
         Random rnd = new Random();
-        for (int i = 0; i < n; i++) {
-            int cell = i + rnd.nextInt(N--);
+        int array[] = new int[N];
+        for ( int i = 0; i < N; i++ )
+            array[i] = i;
+        for ( int i = 0; i < n; i++ ) {
+            int cell = i + rnd.nextInt( N-- );
+            if ( cell != i ) {
+                int temp = array[i];
+                array[i] = array[cell];
+                array[cell] = temp;
+            }
+            cell = array[i];
+
             int r = cell / cols, c = cell % cols;
             cells[r][c] = MINE;
-            markSurroundingCells(r - 1, c - 1);
-            markSurroundingCells(r - 1, c);
-            markSurroundingCells(r - 1, c + 1);
-            markSurroundingCells(r, c - 1);
-            markSurroundingCells(r, c + 1);
-            markSurroundingCells(r + 1, c - 1);
-            markSurroundingCells(r + 1, c);
-            markSurroundingCells(r + 1, c + 1);
+            markSurroundingCells( r - 1, c - 1 );
+            markSurroundingCells( r - 1, c );
+            markSurroundingCells( r - 1, c + 1 );
+            markSurroundingCells( r, c - 1 );
+            markSurroundingCells( r, c + 1 );
+            markSurroundingCells( r + 1, c - 1 );
+            markSurroundingCells( r + 1, c );
+            markSurroundingCells( r + 1, c + 1 );
         }
-        EventQueue.invokeLater(() -> {
-            for (int r = 0; r < rows; r++) {
-                for (int c = 0; c < cols; c++) {
-                    JButton b = new JButton();
-                    b.setAction(new PushButtonAction(r, c, b));
-                    add(b);
-                }
+        for ( int r = 0; r < rows; r++ ) {
+            for ( int c = 0; c < cols; c++ ) {
+                final JButton b = new JButton();
+                b.setPreferredSize( new Dimension( 32, 32 ) );
+                b.setSize( b.getPreferredSize() );
+                b.setAction( new PushButtonAction( r, c, b ) );
+                add( b );
+                pbs[r][c] = b;
             }
-        });
+        }
+    }
+
+    private boolean isInBoard( int r, int c ) {
+        return r >= 0 && r < rows && c >= 0 && c < cols;
     }
 
     private class PushButtonAction extends AbstractAction {
@@ -75,87 +96,105 @@ public class Ms extends JPanel {
         public final int r, c;
         public final JButton b;
 
-        public PushButtonAction(int r, int c, JButton b) {
+        public PushButtonAction( int r, int c, JButton b ) {
             this.r = r;
             this.c = c;
             this.b = b;
         }
 
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            EventQueue.invokeLater(() -> {
-                System.out.println(format("Button@(%d, %d) pressed\n", r, c));
-                b.setBorderPainted(false);
-                b.setOpaque(false);
-                if (cells[r][c] == MINE) {
-                    b.setBackground(Color.BLACK);
-                    System.out.println("BOUMMMMMM!!!");
-                } else {
-                    b.setBackground(bg[cells[r][c] & MINES_MASK]);
-                }
-                if ((cells[r][c] & MINES_MASK) > 0) {
-                    b.setText(format(
-                            "%d", cells[r][c] & MINES_MASK));
-                    b.setPreferredSize(new Dimension(32, 32));
-                }
+        private void doNeighbor( final int r, final int c, final ActionEvent e ) {
+            if ( isInBoard( r, c ) && (cells[r][c] & ALREADY_OPENED) == 0 ) {
+                pbs[r][c].getAction().actionPerformed( e );
+            }
+        }
 
-            });
+        @Override
+        public void actionPerformed( ActionEvent e ) {
+
+            System.out.println(
+                    format( "Button@(%d, %d) pressed\n", r, c ) );
+            b.setBorderPainted( false );
+
+            if ( cells[r][c] == MINE ) {
+                /* hit a mine */
+                b.setBackground( Color.BLACK );
+                b.setText( "@" );
+                System.out.println( "BOUMMMMMM!!!" );
+            } else {
+                b.setBackground( bg[cells[r][c] & MINES_MASK] );
+                if ( (cells[r][c] & MINES_MASK) > 0 ) {
+                    b.setText( format(
+                            "%d", cells[r][c] & MINES_MASK ) );
+                } else {
+                    cells[r][c] |= ALREADY_OPENED;
+                    EventQueue.invokeLater( () -> {
+                        doNeighbor( r - 1, c - 1, e );
+                        doNeighbor( r - 1, c, e );
+                        doNeighbor( r - 1, c + 1, e );
+                        doNeighbor( r, c - 1, e );
+                        doNeighbor( r, c + 1, e );
+                        doNeighbor( r + 1, c - 1, e );
+                        doNeighbor( r + 1, c, e );
+                        doNeighbor( r + 1, c + 1, e );
+                    } );
+                }
+            }
         }
 
     }
 
-    public Ms(int rows, int cols) {
-        this(rows, cols, DEFAULT_PROB);
+    public Ms( int rows, int cols ) {
+        this( rows, cols, DEFAULT_PROB );
     }
 
     public Ms() {
-        this(DEFAULT_ROWS, DEFAULT_COLS, DEFAULT_PROB);
+        this( DEFAULT_ROWS, DEFAULT_COLS, DEFAULT_PROB );
     }
 
-    private void markSurroundingCells(int r, int c) {
-        if (r >= 0 && r < rows
-                && c >= 0 && c < cols
-                && cells[r][c] != MINE) {
+
+    private void markSurroundingCells( int r, int c ) {
+        if ( isInBoard( r, c ) && cells[r][c] != MINE ) {
             cells[r][c]++;
         }
     }
 
-    void line(StringBuilder sb, int cols) {
-        sb.append("+");
-        for (int c = 0; c < cols; c++) {
-            sb.append("--");
+    void line( StringBuilder sb, int cols ) {
+        sb.append( "+" );
+        for ( int c = 0; c < cols; c++ ) {
+            sb.append( "--" );
         }
-        sb.append("-+\n");
+        sb.append( "-+\n" );
     }
 
+    @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        line(sb, cols);
-        for (int r = 0; r < rows; r++) {
-            sb.append("|");
-            for (int c = 0; c < cols; c++) {
-                if (cells[r][c] == MINE) {
-                    sb.append(" @");
-                } else if (cells[r][c] == 0) {
-                    sb.append("  ");
+        line( sb, cols );
+        for ( int r = 0; r < rows; r++ ) {
+            sb.append( "|" );
+            for ( int c = 0; c < cols; c++ ) {
+                if ( cells[r][c] == MINE ) {
+                    sb.append( " @" );
+                } else if ( cells[r][c] == 0 ) {
+                    sb.append( "  " );
                 } else {
-                    sb.append(format(" %d",
-                            cells[r][c] & MINES_MASK));
+                    sb.append( format( " %d",
+                                       cells[r][c] & MINES_MASK ) );
                 }
             }
-            sb.append(" |\n");
+            sb.append( " |\n" );
         }
-        line(sb, cols);
+        line( sb, cols );
         return sb.toString();
     }
 
-    public static void main(String[] args) {
-        Ms board = new Ms();
-        JFrame frame = new JFrame(Ms.class.getSimpleName());
-        frame.setContentPane(board);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(320, 320);
-        frame.setVisible(true);
-        System.out.println(board);
+    public static void main( String[] args ) {
+        JFrame frame = new JFrame( Ms.class.getSimpleName() );
+        Ms board = new Ms( 16, 16 );
+        frame.setContentPane( board );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setSize( 320, 320 );
+        frame.setVisible( true );
+        System.out.println( board );
     }
 }
